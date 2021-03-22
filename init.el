9b0d22add9a197ea0be3a67b984449b76e41f417
Pablo Fonseca <pablofonseca777@gmail.com>
2021-03-21 23:19:12 -0300

    remove duplicated section

--- init.el
+++ init.el
@@ -1167,6 +1167,3 @@
   (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode)))
 
 (use-package ripgrep)
-
-;; Make gc pauses faster by decreasing the threshold
-(setq gc-cons-threshold (* 2 1000 1000))
